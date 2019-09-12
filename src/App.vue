487c1634ecc5aaffbe4d4ca43978174c8e18f386
sailesh-mahat <sailesharsenal@gmail.com>
2019-09-12 16:30:25 +0200

    final app, added some styling with vue material

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,103 @@
 <template>
   <div>
-    <input v-model="currentTodo" @keydown.enter="addTodo(todo)" placeholder="Add a todo">
-    <ul class="todos">
-      <li v-for="(todo, index) in todos" :key="todo.id">
-        <input type='checkbox' v-model='todo.completed' >
-            <div>
-              <span
-                class="todo-item-label"
-                :class='{completed: todo.completed}'
-                @dblclick='editTodo(todo)'
-                v-if="!todo.edit">
-                  {{ todo.label }}
-              </span>
-                <input v-else class="todo-item-edit" type="text" v-model='todo.label'>
-              </div>
-          <button @click="removeTodo(index)">Delete</button>
-      </li>
-    </ul>
+  <h2>To-Do List</h2>
+    <md-card class="cardBox">
+      <section>
+        <div class="todoBox">
+          <li v-for="(todo, index) in todos" :key="todo.id">
+            <md-checkbox v-model='todo.completed' class="md-primary"></md-checkbox>
+
+                  <span
+                    class="todo-item-label"
+                    :class='{completed: todo.completed}'
+                    @dblclick='editTodo(todo)'
+                    v-if="!todo.edit">
+                      {{ todo.label }}
+                  </span>
+                    <md-input v-else class="todo-item-edit" type="text" v-model='todo.label' @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.escape="doneEdit(todo)">
+                    </md-input>
+              <md-button class="md-accent" @click="removeTodo(index)">Delete</md-button>
+          </li>
+      </div>
+      <md-field>
+        <md-input v-model="currentTodo" @keydown.enter="addTodo(todo)" placeholder="Add a todo"></md-input>
+      </md-field>
+    </section>
+    </md-card>
   </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
     todos: [
-    {
-      'id': 1,
-      'label': 'trash',
-      'completed': false,
-      'edit': false
-    },
       {
-      'id': 2,
-      'label': 'dishes',
-      'completed': false,
-      'edit': false
-    },
-  ],
-    currentTodo: '',
-    editedTodo: null
+        'id': 1,
+        'label': 'pay bills',
+        'completed': false,
+        'edit': false
+      },
+        {
+        'id': 2,
+        'label': 'go jogging',
+        'completed': false,
+        'edit': false
+      },
+    ],
+      currentTodo: '',
+      editedTodo: null
     };
   },
   methods: {
     addTodo() {
+      if (this.currentTodo.trim() == 0) {
+            return
+      }
       this.todos.push({id: this.todos.length, label: this.currentTodo, completed: false, edit: false});
       this.currentTodo = '';
-    }
-    removeTodo(todo) {
-      var index = this.todos.indexOf(todo);
+    },
+    removeTodo(index) {
       this.todos.splice(index, 1);
-      },
+    },
     editTodo(todo) {
       todo.edit = true
+    },
+    doneEdit(todo) {
+      todo.edit = false
     }
   }
 };
-
 </script>
 
 <style>
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Roboto', Helvetica, Arial, sans-serif;
+  }
+
+li {
+  list-style: none;
+  }
+
+.container {
+  width: 50%;
+  margin: auto;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+h2{
+  text-align: center;
+}
+
 .completed {
   text-decoration: line-through;
+}
+
+.todoBox {
+  font-size: 1.5em;
+}
+.cardBox {
+  padding: 90px;
 }
 </style>
